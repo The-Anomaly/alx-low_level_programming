794fa373491be0e4379a0a893e830275588374c2
The-Anomaly <favouradekogbe@gmail.com>
2022-03-14 23:17:09 +0000

    alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
