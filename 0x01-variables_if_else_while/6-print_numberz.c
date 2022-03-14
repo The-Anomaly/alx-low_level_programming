bc50f33e7cdecb904211cbfd48904c7299336891
The-Anomaly <favouradekogbe@gmail.com>
2022-03-14 23:50:33 +0000

    print single digits with ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - print single digits
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
