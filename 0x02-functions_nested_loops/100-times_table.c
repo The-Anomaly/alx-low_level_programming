1934b36f38d84cb2d71b8865624d562a97103fb5
The-Anomaly <favouradekogbe@gmail.com>
2022-03-16 23:18:35 +0000

    update times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j, product;
 
-	if (n < 15 && n > 0)
+	if (n < 15 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -23,7 +23,6 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
 				}
 				else if (product > 9)
 				{
@@ -31,7 +30,6 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
 				}
 				else
 				{
@@ -42,8 +40,8 @@ void print_times_table(int n)
 						_putchar(' ');
 						_putchar(' ');
 					}
-					_putchar((product % 10) + '0');
 				}
+				_putchar((product % 10) + '0');
 			}
 			_putchar(10);
 		}
