e4c57f372bb3bd1317665d09b0a09a432e7e7692
The-Anomaly <favouradekogbe@gmail.com>
2022-03-16 21:47:28 +0000

    fix character count

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,18 +13,14 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			if ((j * i) / 10 > 0)
-			{
 				_putchar((j * i) / 10 + '0');
-			}
+			else if (j != 0)
+				_putchar(' ');
 			_putchar((j * i) % 10 + '0');
 			if (j != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if ((j * i) / 10 == 0)
-				{
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar(10);
