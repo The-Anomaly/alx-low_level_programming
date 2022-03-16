b6c419fd04aea8d01d401a23578291043b3717c1
The-Anomaly <favouradekogbe@gmail.com>
2022-03-16 21:35:40 +0000

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* times_table - print the times table
+*
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ((j * i) / 10 > 0)
+			{
+				_putchar((j * i) / 10 + '0');
+			}
+			_putchar((j * i) % 10 + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if ((j * i) / 10 == 0)
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar(10);
+	}
+}
