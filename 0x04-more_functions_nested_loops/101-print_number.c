b41634c024c860be38ccc485ae0a1ac13f361d80
The-Anomaly <favouradekogbe@gmail.com>
2022-03-17 22:12:54 +0000

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,32 +7,26 @@
 */
 void print_number(int n)
 {
-	if (n > 999 && n <= 9999)
+	unsigned int m, count = 1;
+
+	if (n < 0)
 	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar((((n % 1000) % 100) / 10) + '0');
-		_putchar((((n % 1000) % 100) % 10) + '0');
-	}
-	else if (n > 99)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar((n % 100) % 10 + '0');
-	}
-	else if (n > 9)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 0)
-	{
-		_putchar(n + '0');
+		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
-		_putchar('-');
-		_putchar((n * -1) / 10 + '0');
-		_putchar((n * -1) % 10 + '0');
+		m = n;
+	}
+
+	while (m > 9)
+	{
+		m /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((n / count) % 10) + '0');
 	}
 }
