ffc156c7ef8da967a6a6212c947b3ea4cfa70fd4
The-Anomaly <favouradekogbe@gmail.com>
2022-03-22 23:59:59 +0000

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcpy - copy a string
+* @src: input string
+* @dest: destination to be copied to
+*
+* Return: string
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (*(src + count) != '\0')
+	{
+		dest[count] = *(src + count);
+		count++;
+	}
+	dest[count + 1] = '\0';
+	return (src);
+}
