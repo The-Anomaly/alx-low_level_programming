48a40fb72dd52efc6c6d4f2415c507cd6323d4c4
The-Anomaly <favouradekogbe@gmail.com>
2022-03-22 01:11:08 +0000

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_rev - print a string in reverse to the stdout
+* @s: input string
+* Return: nothing
+*/
+void print_rev(char *s)
+{
+	char val;
+	int count = 0, lim, i;
+
+	while (val != '\0')
+	{
+		val = *(s + count);
+		count++;
+	}
+
+	lim = count;
+
+	for (i = lim - 1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
