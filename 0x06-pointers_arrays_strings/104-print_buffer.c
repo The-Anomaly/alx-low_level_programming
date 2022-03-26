917b387be783b57f0ba4996d2ce4ad34795edd62
The-Anomaly <favouradekogbe@gmail.com>
2022-03-26 00:35:46 +0000

    update buffer script

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,42 +2,45 @@
 #include <stdio.h>
 
 /**
-* print_buffer - prinf buffer
-* @b: buffer
-* @size: size of the buffer
+* print_buffer - print buffer
+* @b: input buffer
+* @size: buffer size
 */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
+	{
 		printf("\n");
+	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < size; i += 10)
 		{
-			j = i;
-			if (i % 10 == 0)
+			printf("%08x: ", i);
+
+			for (j = i; j < i + 10; j++)
 			{
-				printf("%08x: ", i);
-				while (j < (i + 10))
+				if (j < size)
+					printf("%02x", b[j]);
+				else
+					printf("  ");
+				if ((j + 1) % 2 == 0)
+					printf(" ");
+			}
+
+			for (k = i; k < i + 10; k++)
+			{
+				if (k < size)
 				{
-					if (j < size)
-						printf("%02x", b[j]);
+					if (b[k] >= 0 && b[k] <= 31)
+						printf("%c", '.');
 					else
-						printf("  ");
-					if ((j + 1) % 2 == 0)
-						printf(" ");
-					j++;
+						printf("%c", b[k]);
 				}
 			}
-			if (b[i] >= 0 && b[i] <= 31)
-				printf("%c", '.');
-			else
-				printf("%c", b[i]);
-			if ((i + 1) % 10 == 0)
-				printf("\n");
-		}
 		printf("\n");
+		}
 	}
 }
