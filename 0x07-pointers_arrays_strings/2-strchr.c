ae2a70c061890650035890b1e7df052f9eb12b50
The-Anomaly <favouradekogbe@gmail.com>
2022-03-28 23:29:38 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: input string
+* @c: character
+*
+* Return: first occurrence of the character c in the string s,
+* or NULL if the character is not found
+*/
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	return (0);
+}
