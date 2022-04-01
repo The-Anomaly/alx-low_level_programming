906624722e8c68096240e285e8dfca1e49b5d4dc
The-Anomaly <favouradekogbe@gmail.com>
2022-04-01 12:01:00 +0000

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - main function
+* @argc: number of arguments
+* @argv: pointer to an array of arguments
+*
+* Return: Always success (0)
+*/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
