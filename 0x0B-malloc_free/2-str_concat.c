0f425dbc988a3d0625bc59be2720a06e4293a7db
The-Anomaly <favouradekogbe@gmail.com>
2022-04-05 12:49:53 +0000

    update str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,31 +9,38 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, k, l = 0;
+	int i = 0, j = 0, k = 0, l = 0, len = 0;
 	char *cat;
 
-	while (s1[i] != '\0' && s1 != NULL)
-	{
-		i++;
-	}
+	if (s1 != NULL)
+		while (s1[i] != '\0')
+		{
+			i++;
+		}
 
-	while (s2[j] != '\0' && s2 != NULL)
-	{
-		j++;
-	}
+	if (s2 != NULL)
+		while (s2[j] != '\0')
+		{
+			j++;
+		}
 
-	cat = malloc(sizeof(char) * (i + j - 1));
+	if (i > 0 && j > 0)
+		len = i + j - 1;
+	else
+		len = i + j;
+
+	cat = malloc(sizeof(char) * len);
 
 	if (cat == NULL)
 		return (NULL);
 
 	for (k = 0; k < i + j; k++)
 	{
-		if (k < i)
+		if (k < i && s1 != NULL)
 		{
 			cat[k] = s1[k];
 		}
-		else
+		else if (s2 != NULL)
 		{
 			cat[k] = s2[l];
 			l++;
