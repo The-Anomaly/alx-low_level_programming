dcaa44e88c26245d9094d6c31e4bfd308ab4aaa4
The-Anomaly <favouradekogbe@gmail.com>
2022-04-05 14:47:10 +0000

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* free_grid - frees a 2D array
+* @grid: 2D array
+* @height: array height
+*/
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
