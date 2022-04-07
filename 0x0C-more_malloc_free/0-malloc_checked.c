b1707372cb58c196fc380b2675234eeeee335c8b
The-Anomaly <favouradekogbe@gmail.com>
2022-04-07 20:26:05 +0000

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* malloc_checked - allocate memory with malloc
+* @b: memory size
+* Return: pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		free(mem);
+		exit(98);
+	}
+	return (mem);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,6 @@
 #include <stdlib.h>
 
 int _putchar(char);
-
+void *malloc_checked(unsigned int b);
 
 #endif
