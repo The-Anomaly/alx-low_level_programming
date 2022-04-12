45bb89db9f93b66fb87aa24eafaa2f91a45cb1a7
The-Anomaly <favouradekogbe@gmail.com>
2022-04-12 22:19:36 +0000

    initilaize struct variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+* init_dog - initialize dog
+* @d: dog struct
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
