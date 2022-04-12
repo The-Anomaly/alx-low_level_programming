562d265cce7c2083f6e2ec10aa22640647dfd801
The-Anomaly <favouradekogbe@gmail.com>
2022-04-12 23:29:42 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+* new_dog - create a new function
+* @name: dog name
+* @age: dog age
+* @owner: dog owner
+*
+* Return: pointer to a struct
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogg;
+	char *namee = name;
+	char *ownerr = owner;
+
+	dogg = malloc(sizeof(dog_t));
+	if (dogg == NULL)
+		return (NULL);
+
+	dogg->name = namee;
+	dogg->age = age;
+	dogg->owner = ownerr;
+
+	return (dog_t);
+}
