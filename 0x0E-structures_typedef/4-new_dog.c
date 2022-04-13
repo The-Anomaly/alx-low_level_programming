2e30800ce78650d0ab4a927be8f9b4ff8c714dda
The-Anomaly <favouradekogbe@gmail.com>
2022-04-13 00:01:09 +0000

    create local copy - new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+
+/**
+* _strcpy - create copy of a string
+* @s: input string;
+* Return: string copy
+*/
+char *_strcpy(char *s)
+{
+	char *new_str;
+	int i = 0, j;
+
+	if (s == NULL)
+		return (NULL);
+
+	while (s[i] != '\0')
+		i++;
+
+	new_str = malloc(sizeof(char) * (i + 1));
+	if (new_str == NULL)
+		return (NULL);
+
+	for (j = 0; s[j] != '\0'; j++)
+		new_str[j] = s[j];
+
+	new_str[j] = '\0';
+	return (new_str);
+}
+
+
 /**
 * new_dog - create a new function
 * @name: dog name
@@ -13,8 +42,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogg;
-	char *namee = name;
-	char *ownerr = owner;
+	char *namee, *ownerr;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
 	dogg = malloc(sizeof(dog_t));
 	if (dogg == NULL)
@@ -23,8 +54,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	namee = _strcpy(name);
+	if (namee == NULL)
+	{
+		free(dogg);
+		return (NULL);
+	}
 	dogg->name = namee;
 	dogg->age = age;
+
+	ownerr = _strcpy(owner);
+	if (ownerr == NULL)
+	{
+		free(dogg->name);
+		free(dogg);
+		return (NULL);
+	}
 	dogg->owner = ownerr;
 
 	return (dogg);
