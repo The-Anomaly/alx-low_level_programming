39e685f0111b6e9666bc6f7cd1d1cc38b6d53399
The-Anomaly <favouradekogbe@gmail.com>
2022-04-25 10:36:58 +0000

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_listint - frees a linked list
+* @head: input list
+*
+*/
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+	{
+		free_listint(head->next);
+	}
+	free(head);
+}
