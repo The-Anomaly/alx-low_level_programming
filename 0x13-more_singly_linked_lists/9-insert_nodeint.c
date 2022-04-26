ab8e1524dad86e0f0a1ea11a18a468680068de4e
The-Anomaly <favouradekogbe@gmail.com>
2022-04-26 22:15:49 +0000

    update insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
 * insert_nodeint_at_index - inserts a new node at a given position
@@ -22,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0 && temp != NULL)
 	{
 		newlist->next = temp;
-		temp = newlist;
+		*head = newlist;
 		return (*head);
 	}
 
