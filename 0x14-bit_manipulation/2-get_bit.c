6506901a864fe6cfc9c1dd72430ad44f5a9e91d1
The-Anomaly <favouradekogbe@gmail.com>
2022-04-29 01:19:24 +0000

    update get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,12 +11,15 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (n == 0 && index < 64)
+		return (0);
+
 	while (n > 0)
 	{
 		if (i == index)
 			return (n & 1);
 		i++;
-		n /= 2;
+		n = n >> 1;
 	}
 	return (-1);
 }
