566a22a3c6b3dd5911908479daff7b96022abe56
The-Anomaly <favouradekogbe@gmail.com>
2022-06-10 01:21:48 +0000

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,38 +11,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-	dlistint_t *new_node, *next, *temp = *h;
+	dlistint_t *tmp = *h, *new;
 
-	if (!*h)
-		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
-	while (temp->prev)
-		temp = temp->prev;
-
-	while (temp)
+	for (; idx != 1; idx--)
 	{
-		if (count == idx - 1)
-		{
-			next = temp->next;
-			temp->next = new_node;
-			new_node->prev = temp;
-			new_node->next = next;
-			return (new_node);
-		}
-
-		count++;
-		temp = temp->next;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 
-	return (NULL);
-}
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
+}
